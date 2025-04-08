fef4e097af9274ea112122e8dd87f765a41f9c97
Lachlan Hurst <lachlan.hurst@gmail.com>
2025-04-08 20:08:06 +1000

    refactor: moved the updatethread to its own file. Renamed it to UpdateThread from UpdateSimThread as it does more than just the similation.

--- src/so100_mujoco_sim/simulate.py
+++ src/so100_mujoco_sim/simulate.py
@@ -1,9 +1,8 @@
 import pathlib
-import time
 
 import mujoco
 import numpy as np
-from PySide6.QtCore import QSettings, Qt, QThread, Signal, Slot
+from PySide6.QtCore import QSettings, Qt, Signal, Slot
 from PySide6.QtGui import QGuiApplication
 from PySide6.QtWidgets import (
     QApplication, QComboBox, QFileDialog, QGroupBox,
@@ -13,135 +12,11 @@ from PySide6.QtWidgets import (
 )
 
 from so100_mujoco_sim.arm_control import (
-    ArmController, Joint,
-    MujocoArmController,
-    So100ArmController, UiArmController,
-    joints_from_model, positions_aligned,
-    update_from_controller
+    Joint,
+    joints_from_model
 )
 from so100_mujoco_sim.mujoco_viewport import Viewport
-
-
-class UpdateSimThread(QThread):
-
-    update_ui_joint_values = Signal(list)
-    update_controller_enabled_states = Signal()
-
-    def __init__(self, model: mujoco.MjModel, data: mujoco.MjData, parent=None) -> None:
-        super().__init__(parent)
-        self.model = model
-        self.data = data
-        self.mujoco_controller = MujocoArmController(model, data)
-        # user interface controller (the UI sliders)
-        # reuse the mujoco model joint definition
-        self.ui_controller = UiArmController(self.mujoco_controller.joints, self._update_ui)
-        self.ui_controller.primary = True
-        self._primary_controller_index = 0
-        self.real_controller = So100ArmController()
-
-        self.arm_controllers: list[ArmController] = []
-        self.arm_controllers.append(self.ui_controller)
-        self.arm_controllers.append(self.mujoco_controller)
-        self.arm_controllers.append(self.real_controller)
-
-        # reset the simulation timer
-        self.reset()
-
-        self.running = True
-
-    @property
-    def real_time(self):
-        return time.monotonic_ns() - self.real_time_start
-
-    def get_primary_controller(self) -> ArmController | None:
-        for ac in self.arm_controllers:
-            if ac.primary:
-                return ac
-        return None
-
-    def run(self) -> None:
-        while self.running:
-            # don't step the simulation past real time
-            # without this the sim usually finishes before it's
-            # even visible
-            if self.data.time < self.real_time / 1_000_000_000:
-                # Update the control loop at a 100hz
-                if (time.monotonic_ns() - self.last_robot_update) / 1_000_000_000 >= (1/100):
-                    self.last_robot_update = time.monotonic_ns()
-
-                    if self.get_primary_controller_index() != self._primary_controller_index:
-                        self._set_primary_controller_index(self._primary_controller_index)
-
-                    pc = self.get_primary_controller()
-                    for ac in self.arm_controllers:
-                        # update the actual positions of the controller
-                        ac.update()
-                        if ac.primary:
-                            pass
-                        else:
-                            # set each of the secondary controllers set positions to that of
-                            # the primary's output
-                            update_from_controller(pc, ac)
-                            # now send those positions to the controller
-                            ac.set_positions()
-
-                # step the simulation
-                mujoco.mj_step(self.model, self.data)
-
-            else:
-                time.sleep(0.00001)
-
-    def stop(self):
-        self.running = False
-        self.wait()
-
-    def reset(self):
-        self.real_time_start = time.monotonic_ns()
-        self.last_robot_update = time.monotonic_ns()
-        self.mujoco_controller.reset()
-
-    def set_joint_position(self, joint_name: str, position: float) -> None:
-        self.ui_controller.set_joint_set_position(joint_name, position)
-
-    def get_controller_names(self) -> str:
-        return [c.name for c in self.arm_controllers]
-
-    def get_controllable_controllers(self) -> list[bool]:
-        return [c.controllable for c in self.arm_controllers]
-
-    def get_primary_controller_index(self) -> int:
-        for i, c in enumerate(self.arm_controllers):
-            if c.primary:
-                return i
-        return 0
-
-    def set_primary_controller_index(self, index: int) -> int:
-        # we need to make sure the primary flag on the controllers
-        # is changed in the main update loop as  setting this flag
-        # can send commands to the motors (which may be getting sent
-        # data from the update thread). So set the desired index
-        # here, and do the proper update for thread in the following fn
-        self._primary_controller_index = index
-
-    def _set_primary_controller_index(self, index: int) -> int:
-        for i, c in enumerate(self.arm_controllers):
-            if i == index:
-                c.primary = True
-            else:
-                c.primary = False
-
-    def connect_real_robot(self, usb_port: str, calibration_folder: str) -> None:
-        self.real_controller.connect(usb_port, calibration_folder)
-        self.real_controller.update()
-
-        # this updates the ui, but also raises change events that causes the mujoco
-        # model to update
-        self.update_ui_joint_values.emit(self.real_controller.joint_output_positions)
-        # raise event to tell UI that the real robot controller can be enabled
-        self.update_controller_enabled_states.emit()
-
-    def _update_ui(self) -> None:
-        self.update_ui_joint_values.emit(self.ui_controller.joint_set_positions)
+from so100_mujoco_sim.update_thread import UpdateThread
 
 
 class JointWidget(QWidget):
@@ -223,7 +98,7 @@ class Window(QMainWindow):
         w.setLayout(layout)
         self.setCentralWidget(w)
 
-        self.th = UpdateSimThread(self.model, self.data, self)
+        self.th = UpdateThread(self.model, self.data, self)
         self.th.update_ui_joint_values.connect(self._update_ui_joint_values)
         self.th.update_controller_enabled_states.connect(self._update_controllers_enabled)
 
